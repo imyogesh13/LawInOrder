9f84cadb94db3752c9eba07d229c6e9f0bda9957
Yogesh Patil <imyogesh13@outlook.com>
2020-08-07 18:49:25 +0530

    Add project files.

---
+++ LawInOrder.API/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LawInOrder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllTimeList()
+        {
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult SaveTimeData([FromBody])
+        {
+            return Ok();
+        }
+    }
+}
